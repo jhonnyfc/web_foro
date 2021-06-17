53faa097d391e2f8d73b1f5f13adae2bde78676b
jhonnyfc <jhonnyfc88@gmail.com>
2021-06-17 09:33:02 +0200

    se han limpiado los archivos inecesarios

--- Practicas/Practica5/eje2.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("x1").value = prompt("Como te llamas?")
-
-document.getElementById("x2").value =  prompt("Cual es tu edad?")
-
-document.getElementById("x3").value = prompt("En que trabajas")
